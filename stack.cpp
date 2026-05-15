fbc7cf2e110777ce33302d9cb497ac541e2d3ca9
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-05-15 14:34:53 +0700

    pop operation

--- stack.cpp
+++ stack.cpp
@@ -32,5 +32,9 @@ class Stack
         newNode->data = value;      // 2. assign value
         newNode->next = top;        // 3. set the next pointer of the new node
         top = newNode;              // 4. Update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
     }
+
+    // pop operation : remove the topmost element from the stack
 };
