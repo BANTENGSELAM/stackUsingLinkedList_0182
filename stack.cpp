d4b9b80003bc4c31c158f218b95da963c011e9ea
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-05-15 14:33:47 +0700

    Update the top pointer to the new node

--- stack.cpp
+++ stack.cpp
@@ -31,5 +31,6 @@ class Stack
         Node *newNode = new Node(); // 1. Allocates memory for the new node
         newNode->data = value;      // 2. assign value
         newNode->next = top;        // 3. set the next pointer of the new node
+        top = newNode;              // 4. Update the top pointer to the new node
     }
 };
